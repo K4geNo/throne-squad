d38240eaef09fd6de7ec0ad3b7bfd5429a16f054
KageNo <micaraujo71@gmail.com>
2024-09-20 18:39:49 -0300

    Add authentication environment variables

---
+++ src/lib/env.ts
@@ -0,0 +1,11 @@
+import { z } from "zod"
+
+const envSchema = z.object({
+	// DATABASE_URL: z.string(),
+	AUTH_SECRET: z.string(),
+	AUTH_DISCORD_ID: z.string(),
+	AUTH_DISCORD_SECRET: z.string().optional(),
+	NODE_ENV: z.string().default("development"),
+})
+
+export const env = envSchema.parse(process.env)
