e1560b9f2b6bf4f26f77e4ebf3d53a26e3c4b8dd
KageNo <micaraujo71@gmail.com>
2024-09-21 21:43:05 -0300

    Add private pages for Dungeons and Register

---
+++ src/app/(private)/page.tsx
@@ -0,0 +1,11 @@
+import SignOutButton from "@/components/sign-out-button"
+
+export default function DungeonsPage() {
+	return (
+		<main>
+			<h1>Dungeons Page</h1>
+
+			<SignOutButton />
+		</main>
+	)
+}
---
+++ src/app/(private)/register/page.tsx
@@ -0,0 +1,11 @@
+import SignOutButton from "@/components/sign-out-button"
+
+export default function RegisterPage() {
+	return (
+		<main>
+			<h1>Register Page</h1>
+
+			<SignOutButton />
+		</main>
+	)
+}
