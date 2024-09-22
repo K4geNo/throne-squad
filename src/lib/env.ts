5dd9ada2ec308e19afc8c865e1c55217468fd45a
KageNo <micaraujo71@gmail.com>
2024-09-21 21:42:31 -0300

    Add DATABASE_URL environment variable to env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 const envSchema = z.object({
-	// DATABASE_URL: z.string(),
+	DATABASE_URL: z.string(),
 	AUTH_SECRET: z.string(),
 	AUTH_DISCORD_ID: z.string(),
 	AUTH_DISCORD_SECRET: z.string().optional(),
